ad00b1f5fbd7f7dfea748fccbab664259db27d50
SadeEtensh <eteneshsade550305@gmail.com>
2021-10-19 12:27:21 +0100

    Updated

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Home'
-import Sade from './src/Sade'
+
 
 export default function App() {
   
   return (
     <view>
       <Home />
-      <Sade />
+     
       </view>
   );
   
