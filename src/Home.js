77c976c08440c706cbed21fe078f9502d27573cf
SadeEtensh <eteneshsade550305@gmail.com>
2021-10-19 12:23:02 +0100

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+const Home = () => {
+    return (
+        <View style={{
+            backgroundColor:"#FEA",
+            flex:1,
+            alignItems:"center",
+            justifyContent:"center "
+        }}>
+            <Text>Hello React native</Text>
+        </View>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
---
+++ src/Sade.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {StyleSheet, Text, View, } from 'react-native' 
+
+
+const Sade = ()=>{
+    const msg = "Sadee";
+    return (
+    <View>
+        <Text>Hello from {msg}</Text>
+    </View>)
+}
+
+export default Sade
